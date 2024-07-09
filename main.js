cd1c4b0a367a0fa9556a4ef815125d0e8982b7f5
Jesse <akohgodwinjesse@gmail.com>
2024-07-10 00:00:19 +0100

    phase in- phase out

--- main.js
+++ main.js
@@ -3,71 +3,79 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import Stats from "stats.js";
 import * as dat from "dat.gui";
 import { gsap } from "gsap";
 
+
+let scene, camera, renderer, controls, stats;
+let gltfloader = new GLTFLoader();
+const dLoader = new DRACOLoader();
+dLoader.setDecoderPath("https://www.gstatic.com/draco/versioned/decoders/1.5.6/");
+dLoader.setDecoderConfig({ type: "js" });
+gltfloader.setDRACOLoader(dLoader);
+
+const clock = new THREE.Clock();
+let pointsObjects = [];
+let mixers = [];
+
+const models = [
+    { name: "Man", path: "./3.glb" },
+    { name: "Man2 same man", path: "./3.glb" },
+    // { name: "Building", path: "./6.glb" },
+    // { name: "Scene 2", path: "./7.glb" },
+    // { name: "Scene 3", path: "./4.glb" },
+
+];
+let currentModelIndex = 0;
+  
 const params = {
     circleRadius: 0,
     circleSpeed: 0,
     maxDisplacement: 0,
 };
 
-let scene, camera, renderer, controls, stats;
-let gltfloader = new GLTFLoader();
-const clock = new THREE.Clock();
-let pointsObjects = [];
-
 window.onload = () => {
     init();
     measure();
-    loadModels();
+    loadModel(models[currentModelIndex].path);
     setupStats();
     setupGUI();
-    setupButton();
     render();
 };
 
-function setupButton() {
-    const button = document.getElementById("resetButton");
-    button.addEventListener("click", () => {
-        resetAnimation();
-    });
-}
 
 function resetAnimation() {
     gsap.to(params, {
         circleRadius: 0,
         circleSpeed: 0,
         maxDisplacement: 0,
+        duration: 2.5,
+        ease:"circ.out"
+    })
+
+    gsap.to(camera.position, {
+        y: 2,
+        z: 7,
+        x: 0,
+        ease:"circ.inOut",
         duration: 2,
-        ease: "power2.inOut",
-        onComplete: () => {
-            pointsObjects.forEach(({ points, originalPositions }) => {
-                gsap.to(points.position, {
-                    x: 0,
-                    duration: 0.5,
-                });
-                gsap.to(points.rotation, {
-                    y: 1.5,
-                    duration: 1,
-                });
-            });
-        },
-    });
+        delay:.5
+    })
 }
 
 function init() {
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000);
-    camera.position.set(0, -2, 5);
+    camera.position.set(0, 2, 5);
 
     scene = new THREE.Scene();
     scene.add(camera);
     scene.position.y = -1;
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor("black");
+    renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
     renderer.toneMappingExposure = 0.6;
     document.body.appendChild(renderer.domElement);
@@ -79,93 +87,58 @@ function init() {
     controls.lookSpeed = 0.09;
 
     controls.autoRotate = true;
-    controls.autoRotateSpeed = 2;
+    controls.autoRotateSpeed = -1;
 }
 
 function setLighting() {
-    const ambientLight = new THREE.AmbientLight("white", 3);
+    const ambientLight = new THREE.AmbientLight("white", 5);
     scene.add(ambientLight);
-    const directionalLight = new THREE.DirectionalLight("lightblue", 3);
+    const directionalLight = new THREE.DirectionalLight("lightblue", 1);
     directionalLight.position.y = 3;
     directionalLight.position.x = -3;
     scene.add(directionalLight);
 }
 
 function measure() {
-    const GridHelper = new THREE.GridHelper(100, 100, new THREE.Color("lime"), new THREE.Color("orange"));
-    scene.add(GridHelper);
+    const GridHelper = new THREE.GridHelper(30, 30, new THREE.Color("red"), new THREE.Color("black"));
+    // scene.add(GridHelper);
 }
 
-function loadModels() {
-    gltfloader.load("./3.glb", (gltf) => {
+function bringToBase(model, offset) {
+    const box = new THREE.Box3().setFromObject(model);
+    const heightOffset = box.min.y;
+    model.position.y -= heightOffset - offset;
+  }
+
+
+function loadModel(path) {
+    gltfloader.load(path, (gltf) => {
         const model = gltf.scene;
         scene.add(model);
         model.traverse(function (child) {
-            if (child.isPoints) {
+            if (child.isMesh | child.isPoints) {
                 const vertices = child.geometry.attributes.position.array;
-                const points1 = new THREE.Points(child.geometry, new THREE.PointsMaterial({ color: "red", size: 0.001 }));
+                const points = new THREE.Points(child.geometry, new THREE.PointsMaterial({size: 0.01 , color:"lime"}));
 
                 const originalPositions = [];
                 for (let i = 0; i < vertices.length; i += 3) {
                     originalPositions.push(new THREE.Vector3(vertices[i], vertices[i + 1], vertices[i + 2]));
                 }
-
-                scene.add(points1);
-                pointsObjects.push({ points: points1, originalPositions });
-
-                scene.add(points1);
-                points1.position.x = 1;
-                points1.rotation.y = -1;
-                const points2 = new THREE.Points(child.geometry, new THREE.PointsMaterial({ color: "lime", size: 0.001 }));
-                scene.add(points2);
-                pointsObjects.push({ points: points2, originalPositions });
-                points2.rotation.y = 2;
-                points2.position.x = -1;
+                pointsObjects.push({ model: model, points: points, originalPositions });
                 child.userData.vertices = vertices;
-                child.userData.points = points1;
-                child.userData.points = points2;
-            } else if (child.isObject3D) {
-                const randomColor = new THREE.Color("pink");
-                if (child.material) {
-                    child.material.color = randomColor;
-                } else if (child.children.length > 0) {
-                    child.children.forEach((subChild) => {
-                        if (subChild.material) {
-                            subChild.material.color = randomColor;
-                        }
-                    });
-                }
-            }
+                child.userData.points = points
+            } 
         });
-    });
-}
 
-const minSize = 0.0;
-const maxSize = 0.5;
-const speed = 4;
-let previousSign = 1;
-let colorChangeCounter = 0;
-const colorChangeInterval = 10;
-
-function animatePointSize(elapsedTime) {
-    const size = minSize + (maxSize - minSize) * (0.5 * (1 + Math.sin(elapsedTime * speed)));
-    const currentSign = Math.sin(elapsedTime * speed);
-
-    scene.traverse(function (child) {
-        if (child.isPoints) {
-            child.material.size = size;
-
-            colorChangeCounter++;
-            if (Math.sign(currentSign) !== Math.sign(previousSign) && colorChangeCounter >= colorChangeInterval) {
-                const randomColor = new THREE.Color(Math.random(), Math.random() * Math.random(), Math.random());
-                child.material.color = randomColor;
-                colorChangeCounter = 0;
-            }
+        if (gltf.animations[0]) {
+            let mixer = new THREE.AnimationMixer(model);
+            mixers.push(mixer);
+            let action = mixer.clipAction(gltf.animations[0]);
+            action.play();
         }
     });
-
-    previousSign = currentSign;
 }
+
 
 function animateCircularBrownianMotion(elapsedTime) {
     pointsObjects.forEach(({ points, originalPositions }) => {
@@ -199,9 +172,13 @@ function render() {
     stats.begin();
 
     const elapsedTime = clock.getElapsedTime();
-
-    animatePointSize(elapsedTime);
     animateCircularBrownianMotion(elapsedTime);
+
+    for (let i = 0; i < mixers.length; i++) {
+        if (mixers[i]) {
+            mixers[i].update(delta);
+        }
+    }
 
     renderer.render(scene, camera);
     requestAnimationFrame(render);
@@ -219,8 +196,62 @@ function setupGUI() {
     const gui = new dat.GUI();
     gui.add(params, "circleRadius", 0, 20).name("Circle Radius");
     gui.add(params, "circleSpeed", 0, 10).name("Circle Speed");
-    gui.add(params, "maxDisplacement", 0, 5).name("Displacement");
+    gui.add(params, "maxDisplacement", 0, 3.5).name("Displacement");
+     const modelNames = models.map(model => model.name);
+    gui.add({ selectedModel: modelNames[0] }, 'selectedModel', modelNames)
+        .name("Select Model")
+        .onChange((value) => {
+            const index = modelNames.indexOf(value)
+            switchModels(index)
+        });
+    gui.add({reset: resetAnimation}, 'reset').name("Merge")
 }
+
+function switchModels(index) {
+    if (pointsObjects.length > 0) {
+        animateParticlesOut(() => {
+            pointsObjects.forEach(({ model, points }) => {
+                scene.remove(model);
+                points.geometry.dispose();
+                points.material.dispose();
+            });
+            loadModel(models[index].path);
+            resetAnimation()
+            pointsObjects = [];
+     
+        });
+    } else {
+        loadModel(models[index].path);
+        resetAnimation()
+    }
+}
+
+function animateParticlesOut(onComplete) {
+    gsap.to(params, {
+        circleRadius: 8,
+        circleSpeed: 6,
+        maxDisplacement: 4,
+        duration: 3,
+        ease:"power1.in",
+        onComplete: () => {
+            gsap.to(params, {
+                maxDisplacement: 10,
+                circleRadius: 70,
+                duration: 1,
+                ease:"power1.inOut",
+                onComplete: onComplete
+            })
+        }
+    })
+
+    gsap.to(camera.position, {
+        y: 20,
+        z: 20,
+        ease:"power2.inOut",
+        duration: 3,
+    })
+}
+
 
 window.addEventListener("resize", () => {
     camera.aspect = window.innerWidth / window.innerHeight;
